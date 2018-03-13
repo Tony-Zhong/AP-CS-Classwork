2942ef4a68babadd0fb417869b15b86d2c6cecf4
Tony_Zhong <tony99zhong@sina.cn>
2018-03-13 10:57:11 +0800

    Shape

---
+++ Class Notes/Shape.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Shape {
+
+
+    public abstract class abstractclass {
+        public abstract double getArea();
+
+        public abstract double getPerimeter();
+    }
+
+    public static void main(String[] args) {
+        Shape a = new circle(11.1);
+
+    }
+}
+
+
+
